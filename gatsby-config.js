83124fadfd62c77560919a9d0c169869d456d26d
martinmunillas <martinmunillas@gmail.com>
2021-05-29 14:26:06 -0400

    feature: set-up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
     title: "Quaantum Components",
+    github: "https://github.com/quaantum/Quaantum-Components"
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-styled-components",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
